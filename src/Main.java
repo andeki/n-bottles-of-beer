d5bf02e4d794fdf216815ea07f83050c36b4a996
andeki <143848894+andeki@users.noreply.github.com>
2023-09-18 17:39:31 -0600

    bottles of beer v1

--- src/Main.java
+++ src/Main.java
@@ -6,23 +6,32 @@ public class Main {
         System.out.print("Enter number of bottles to start with: ");
         Scanner s = new Scanner(System.in);
         int total = s.nextInt();
+        System.out.println();
 
         //loop taking amount of bottles and displaying
         for (int i = total; i > 0; i--)
         {
-            System.out.println(i + " bottles of beer on the wall, " + i + " bottles of beer.");
+            //plurality check for i being 1 to say "bottle" rather than "bottles"
 
-            //plurality check for 1 to say "bottle" rather than "bottles"
-            if(i-1 <= 1)
+            if(i == 1)
+            {
+                System.out.println(i + " bottle of beer on the wall, " + i + " bottle of beer.");
+            } else {
+                System.out.println(i + " bottles of beer on the wall, " + i + " bottles of beer.");
+            }
+
+            //plurality check for i-1
+            if(i-1 == 1)
             {
                 System.out.println("Take one and pass it down, " + (i-1) + " bottle of beer on the wall.");
             } else {
                 System.out.println("Take one and pass it down, " + (i-1) + " bottles of beer on the wall.");
             }
+            //gui line
             System.out.println();
         }
 
-        //run output restarting saying
+        //run output ending/asking for restart of the saying
         System.out.println("0 bottles of beer on the wall, 0 bottles of beer.");
         System.out.println("Go to the store and buy some more, 5 bottles of beer on the wall.");
     }
